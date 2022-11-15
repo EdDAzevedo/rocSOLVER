c966210f28da41195ee97bb3af3bfb8253ac7da1
Ed D'Azevedo <98369794+EdDAzevedo@users.noreply.github.com>
2022-11-15 14:03:17 -0500

    add typename type I

--- library/src/gbtr/getrf_npvt_bf.hpp
+++ library/src/gbtr/getrf_npvt_bf.hpp
@@ -8,43 +8,43 @@
 
 #include "gbtr_common.h"
 
-template <typename T>
-DEVICE_FUNCTION void getrf_npvt_bf_device(rocblas_int const batchCount,
-                                          rocblas_int const m,
-                                          rocblas_int const n,
+template <typename T, typename I>
+DEVICE_FUNCTION void getrf_npvt_bf_device(I const batchCount,
+                                          I const m,
+                                          I const n,
                                           T* A_,
-                                          rocblas_int const lda,
-                                          rocblas_int* pinfo)
+                                          I const lda,
+                                          I* pinfo)
 {
-    rocblas_int const min_mn = (m < n) ? m : n;
+    I const min_mn = (m < n) ? m : n;
     T const one = 1;
-    rocblas_int info = 0;
+    I info = 0;
 
 #ifdef USE_GPU
-    rocblas_int const iv_start = (blockIdx.x * blockDim.x + threadIdx.x) + 1;
-    rocblas_int const iv_end = batchCount;
-    rocblas_int const iv_inc = (gridDim.x * blockDim.x);
+    I const iv_start = (blockIdx.x * blockDim.x + threadIdx.x) + 1;
+    I const iv_end = batchCount;
+    I const iv_inc = (gridDim.x * blockDim.x);
 #else
-    rocblas_int const iv_start = 1;
-    rocblas_int const iv_end = batchCount;
-    rocblas_int const iv_inc = 1;
+    I const iv_start = 1;
+    I const iv_end = batchCount;
+    I const iv_inc = 1;
 #endif
 
 #define A(iv, i, j) A_[indx3f(iv, i, j, batchCount, lda)]
 
     T const zero = 0;
 
-    for(rocblas_int j = 1; j <= min_mn; j++)
+    for(I j = 1; j <= min_mn; j++)
     {
-        rocblas_int const jp1 = j + 1;
+        I const jp1 = j + 1;
 
-        for(rocblas_int iv = iv_start; iv <= iv_end; iv += iv_inc)
+        for(I iv = iv_start; iv <= iv_end; iv += iv_inc)
         {
             bool const is_diag_zero = (std::abs(A(iv, j, j)) == zero);
             T const Ujj_iv = is_diag_zero ? one : A(iv, j, j);
             info = is_diag_zero && (info == 0) ? j : info;
 
-            for(rocblas_int ia = jp1; ia <= m; ia++)
+            for(I ia = jp1; ia <= m; ia++)
             {
                 A(iv, ia, j) = A(iv, ia, j) / Ujj_iv;
             };
@@ -52,11 +52,11 @@ DEVICE_FUNCTION void getrf_npvt_bf_device(rocblas_int const batchCount,
 
         SYNCTHREADS;
 
-        for(rocblas_int ja = jp1; ja <= n; ja++)
+        for(I ja = jp1; ja <= n; ja++)
         {
-            for(rocblas_int ia = jp1; ia <= m; ia++)
+            for(I ia = jp1; ia <= m; ia++)
             {
-                for(rocblas_int iv = iv_start; iv <= iv_end; iv += iv_inc)
+                for(I iv = iv_start; iv <= iv_end; iv += iv_inc)
                 {
                     A(iv, ia, ja) = A(iv, ia, ja) - A(iv, ia, j) * A(iv, j, ja);
                 };
