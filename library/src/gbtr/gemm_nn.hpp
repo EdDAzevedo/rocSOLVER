037fa1e13eceb9352dc01adb96b7c05bf114ae5e
Ed D'Azevedo <98369794+EdDAzevedo@users.noreply.github.com>
2022-11-15 14:02:20 -0500

    add typename type I

--- library/src/gbtr/gemm_nn.hpp
+++ library/src/gbtr/gemm_nn.hpp
@@ -10,18 +10,18 @@
 #include "gbtr_common.h"
 #include "gemm_nn_fixed.hpp"
 
-template <typename T>
-DEVICE_FUNCTION void gemm_nn_device(rocblas_int const m,
-                                    rocblas_int const n,
-                                    rocblas_int const k,
+template <typename T, typename I>
+DEVICE_FUNCTION void gemm_nn_device(I const m,
+                                    I const n,
+                                    I const k,
                                     T const alpha,
                                     T const* const A_,
-                                    rocblas_int const lda,
+                                    I const lda,
                                     T const* const B_,
-                                    rocblas_int const ldb,
+                                    I const ldb,
                                     T const beta,
                                     T* C_,
-                                    rocblas_int const ldc)
+                                    I const ldc)
 {
 #define A(ia, ja) A_[indx2f(ia, ja, lda)]
 #define B(ib, jb) B_[indx2f(ib, jb, ldb)]
@@ -54,12 +54,12 @@ DEVICE_FUNCTION void gemm_nn_device(rocblas_int const m,
     T const zero = 0;
     bool const is_beta_zero = (beta == zero);
 
-    for(rocblas_int jc = 1; jc <= n; jc++)
+    for(I jc = 1; jc <= n; jc++)
     {
-        for(rocblas_int ic = 1; ic <= m; ic++)
+        for(I ic = 1; ic <= m; ic++)
         {
             T cij = zero;
-            for(rocblas_int ja = 1; ja <= k; ja++)
+            for(I ja = 1; ja <= k; ja++)
             {
                 cij += A(ic, ja) * B(ja, jc);
             };
