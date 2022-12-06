7f8288d2131faf8ef7d7185346d2882b9b99abbb
Ed D'Azevedo <98369794+EdDAzevedo@users.noreply.github.com>
2022-12-06 11:28:51 -0500

    initial checkin

---
+++ library/src/gbtr/rocsolver_gebltsv_npvt_interleaved_batch.cpp
@@ -0,0 +1,160 @@
+
+
+/*! \file */
+/* ************************************************************************
+ * Copyright (C) 2020-2022 Advanced Micro Devices, Inc. All rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ************************************************************************ */
+#include "rocsolver_gebltsv_npvt_interleaved_batch.hpp"
+
+extern "C" {
+
+rocblas_status rocsolver_dgebltsv_npvt_interleaved_batch(
+                 rocblas_handle handle,
+                 const rocblas_int nb,
+                 const rocblas_int nblocks,
+                 const rocblas_int nrhs,
+
+                 double * A_,
+                 const rocblas_int lda,
+
+                 double * B_,
+                 const rocblas_int ldb,
+
+                 double * C_,
+                 const rocblas_int ldc,
+
+                 double * X_,
+                 const rocblas_int ldx,
+
+                 rocblas_int info_array[],
+                 const rocblas_int batch_count 
+                 )
+{
+ return( rocsolver_gebltsv_npvt_interleaved_batch_impl(
+                 handle, nb, nblocks, nrhs,
+                 A_, lda,   B_, ldb,   C_, ldc,
+                 X_, ldx,
+                 info_array,
+                 batch_count ) );
+}
+
+
+
+
+
+
+rocblas_status rocsolver_sgebltsv_npvt_interleaved_batch(
+                 rocblas_handle handle,
+                 const rocblas_int nb,
+                 const rocblas_int nblocks,
+                 const rocblas_int nrhs,
+
+                 float * A_,
+                 const rocblas_int lda,
+
+                 float * B_,
+                 const rocblas_int ldb,
+
+                 float * C_,
+                 const rocblas_int ldc,
+
+                 float * X_,
+                 const rocblas_int ldx,
+
+                 rocblas_int info_array[],
+                 const rocblas_int batch_count 
+                 )
+{
+ return( rocsolver_gebltsv_npvt_interleaved_batch_impl(
+                 handle, nb, nblocks, nrhs,
+                 A_, lda,   B_, ldb,   C_, ldc,
+                 X_, ldx,
+                 info_array,
+                 batch_count ) );
+}
+
+
+rocblas_status rocsolver_zgebltsv_npvt_interleaved_batch(
+                 rocblas_handle handle,
+                 const rocblas_int nb,
+                 const rocblas_int nblocks,
+                 const rocblas_int nrhs,
+
+                 rocblas_double_complex * A_,
+                 const rocblas_int lda,
+
+                 rocblas_double_complex * B_,
+                 const rocblas_int ldb,
+
+                 rocblas_double_complex * C_,
+                 const rocblas_int ldc,
+
+                 rocblas_double_complex * X_,
+                 const rocblas_int ldx,
+
+                 rocblas_int info_array[],
+                 const rocblas_int batch_count 
+                 )
+{
+ return( rocsolver_gebltsv_npvt_interleaved_batch_impl(
+                 handle, nb, nblocks, nrhs,
+                 A_, lda,   B_, ldb,   C_, ldc,
+                 X_, ldx,
+                 info_array,
+                 batch_count ) );
+}
+
+
+
+
+
+
+rocblas_status rocsolver_cgebltsv_npvt_interleaved_batch(
+                 rocblas_handle handle,
+                 const rocblas_int nb,
+                 const rocblas_int nblocks,
+                 const rocblas_int nrhs,
+
+                 rocblas_float_complex * A_,
+                 const rocblas_int lda,
+
+                 rocblas_float_complex * B_,
+                 const rocblas_int ldb,
+
+                 rocblas_float_complex * C_,
+                 const rocblas_int ldc,
+
+                 rocblas_float_complex * X_,
+                 const rocblas_int ldx,
+
+                 rocblas_int info_array[],
+                 const rocblas_int batch_count 
+                 )
+{
+ return( rocsolver_gebltsv_npvt_interleaved_batch_impl(
+                 handle, nb, nblocks, nrhs,
+                 A_, lda,   B_, ldb,   C_, ldc,
+                 X_, ldx,
+                 info_array,
+                 batch_count ) );
+}
+}
---
+++ library/src/gbtr/rocsolver_gebltsv_npvt_interleaved_batch.hpp
@@ -0,0 +1,74 @@
+
+
+/*! \file */
+/* ************************************************************************
+ * Copyright (C) 2020-2022 Advanced Micro Devices, Inc. All rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ************************************************************************ */
+
+#pragma once
+#ifndef ROCSOLVER_GEBLTSV_NPVT_INTERLEAVED_BATCH_HPP
+#define ROCSOLVER_GEBLTSV_NPVT_INTERLEAVED_BATCH_HPP
+
+#include "geblt_common.h"
+#include "rocsolver_geblttrf_npvt_interleaved_batch.hpp"
+#include "rocsolver_geblttrs_npvt_interleaved_batch.hpp"
+
+template< typename T, typename I>
+rocblas_status rocsolver_gebltsv_npvt_interleaved_batch_impl(
+            rocblas_handle handle,
+            const I nb,
+            const I nblocks,
+            const I nrhs,
+            T * A_,
+            const I lda,
+            T * B_,
+            const I ldb,
+            T * C_,
+            const I ldc,
+            T * X_,
+            const I ldx,
+            I dinfo_array[],
+            const I batch_count
+            )
+{
+
+    rocblas_status istat = rocblas_status_success;
+
+    istat = rocsolver_geblttrf_npvt_interleaved_batch_impl(
+                 handle, nb, nblocks, 
+                 A_, lda, B_, ldb, C_, ldc, 
+                 dinfo_array, 
+                 batch_count );
+    if (istat != rocblas_status_success ) {
+       return(istat);
+       };
+
+    istat = rocsolver_geblttrs_npvt_interleaved_batch_impl(
+               handle, nb, nblocks, nrhs,
+               A_, lda, B_, ldb, C_, ldc,
+               X_, ldx,
+               batch_count );
+    return( istat );
+}
+
+
+#endif
