e2b41de9066f27090ba04a250a533d672eb3ce33
Ed D'Azevedo <98369794+EdDAzevedo@users.noreply.github.com>
2023-01-12 22:46:58 -0500

    initial checkin

---
+++ library/src/refactor/rf_lusolve.hpp
@@ -0,0 +1,171 @@
+
+/*! \file */
+/* ************************************************************************
+ * Copyright (C) 2020-2022 Advanced Micro Devices, Inc. All rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ************************************************************************ */
+#pragma once
+#ifndef RF_LUSOLVE_HPP
+#define RF_LUSOLVE_HPP
+
+
+
+template< typename Iint, typename Ilong, typename T>
+rocsolverStatus_t rf_lusolve(rocsolverRfHandle_t handle,
+                             Iint const n,
+                             Ilong const nnz,
+                             Ilong* const d_LUp,
+                             Iint* const d_LUi,
+                             T* const d_LUx,
+                             T* const d_b,
+                             T* const d_Temp)
+{
+    {
+
+    bool isok = (handle != nullptr) && (handle->hipsparse_handle != nullptr);
+    if (!isok) {
+        return(ROCSOLVER_STATUS_NOT_INITIALIZED );
+        };
+    }
+
+    Iint const m = n;
+    {
+        bool const isok_arg
+            = (d_LUp != nullptr) && (d_LUi != nullptr) && (d_LUx != nullptr) && (d_b != nullptr);
+
+        if(!isok_arg)
+        {
+            return (ROCSOLVER_STATUS_INVALID_VALUE);
+        };
+    };
+
+
+    hipsparseMatDescr_t descrL = handle->descrL;
+
+    csrsv2Info_t infoL = handle->infoL;
+
+    hipsparseMatDescr_t descrU = handle->descrU;
+
+    csrsv2Info_t infoU = handle->infoU;
+
+
+   // --------------------------------
+   // Allocate workspace for hipSPARSE
+   // --------------------------------
+
+    Ilong const lnnz = nnz;
+    size_t bufferSize = 1;
+    int stmp = 0;
+
+    T* const csrSortedValA = d_LUx;
+    Ilong* const csrSortedRowPtrA = d_LUp;
+    Iint* const csrSortedColIndA = d_LUi;
+
+
+    hipsparseSolvePolicy_t policy = (handle->solve_alg  == ROCSOLVERRF_TRIANGULAR_SOLVE_ALG1) ? 
+                                      HIPSPARSE_SOLVE_POLICY_USE_LEVEL : 
+                                      HIPSPARSE_SOLVE_POLICY_NO_LEVEL;
+
+    hipsparseOperation_t transL = HIPSPARSE_OPERATION_NON_TRANSPOSE;
+    hipsparseOperation_t transU = HIPSPARSE_OPERATION_NON_TRANSPOSE;
+
+    stmp = 0;
+    HIPSPARSE_CHECK(hipsparseDcsrsv2_bufferSize(handle, transL, m, lnnz, descrL, csrSortedValA,
+                                                csrSortedRowPtrA, csrSortedColIndA, infoL, &stmp),
+                    ROCSOLVER_STATUS_INTERNAL_ERROR);
+
+    if(stmp > bufferSize)
+    {
+        bufferSize = stmp;
+    }
+
+    stmp = 0;
+    HIPSPARSE_CHECK(hipsparseDcsrsv2_bufferSize(handle, transU, m, lnnz, descrU, csrSortedValA,
+                                                csrSortedRowPtrA, csrSortedColIndA, infoU, &stmp),
+                    ROCSOLVER_STATUS_INTERNAL_ERROR);
+
+    if(stmp > bufferSize)
+    {
+        bufferSize = stmp;
+    }
+
+    T* const d_y = d_Temp;
+    void* const buffer = handle->buffer;
+    {
+    bool isok = ( handle->buffer_size >= bufferSize );
+    assert( isok );
+    if (isok) {
+      return( ROCSOLVER_STATUS_INTERNAL_ERROR );
+      };
+    };
+
+
+
+    T* const d_x = d_b;
+
+    /*
+   -------------------------------------------
+   If A = LU
+   Solve A x = (LU) x b as
+   (1)   solve L y = b,   L unit diagonal
+   (2)   solve U x = y,   U non-unit diagonal
+
+   -------------------------------------------
+   */
+
+    HIPSPARSE_CHECK(hipsparseDcsrsv2_analysis(handle, transL, m, lnnz, descrL, csrSortedValA,
+                                              csrSortedRowPtrA, csrSortedColIndA, infoL, policy,
+                                              buffer),
+                    ROCSOLVER_STATUS_INTERNAL_ERROR);
+
+    HIPSPARSE_CHECK(hipsparseDcsrsv2_analysis(handle, transU, m, lnnz, descrU, csrSortedValA,
+                                              csrSortedRowPtrA, csrSortedColIndA, infoU, policy,
+                                              buffer),
+                    ROCSOLVER_STATUS_INTERNAL_ERROR);
+
+    /*
+     ----------------------
+     step (1) solve L y = b
+     ----------------------
+     */
+
+    T alpha = 1.0;
+
+    HIPSPARSE_CHECK(hipsparseDcsrsv2_solve(handle, transL, m, lnnz, &alpha, descrL, csrSortedValA,
+                                           csrSortedRowPtrA, csrSortedColIndA, infoL, d_b, d_y,
+                                           policy, buffer),
+                    ROCSOLVER_STATUS_INTERNAL_ERROR);
+
+    /*
+          ----------------------
+          step (2) solve U x = y
+          ----------------------
+         */
+
+    HIPSPARSE_CHECK(hipsparseDcsrsv2_solve(handle, transU, m, lnnz, &alpha, descrU, csrSortedValA,
+                                           csrSortedRowPtrA, csrSortedColIndA, infoU, d_y, d_x,
+                                           policy, buffer),
+                    ROCSOLVER_STATUS_INTERNAL_ERROR);
+
+
+    return (ROCSOLVER_STATUS_SUCCESS);
+}
+#endif
---
+++ library/src/refactor/rf_scatter.hpp
@@ -0,0 +1,74 @@
+
+/*! \file */
+/* ************************************************************************
+ * Copyright (C) 2020-2022 Advanced Micro Devices, Inc. All rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ************************************************************************ */
+
+#pragma once
+#ifndef RF_SCATTER_HPP
+#define RF_SCATTER_HPP
+
+#include <hip/hip_runtime.h>
+#include <hip/hip_runtime_api.h>
+#include <assert.h>
+
+#ifndef SCATTER_THREADS
+#define SCATTER_THREADS 1024
+#endif
+
+template <typename Iint, typename T>
+static __global__  void rf_scatter_kernel(
+                                        Iint const n, 
+                                        Iint const * const P, 
+                                        T    const * const src, 
+                                        T          * const dest)
+{
+
+
+    Iint const i_start = threadIdx.x + blockIdx.x * blockDim.x;
+    Iint const i_inc = blockDim.x * gridDim.x;
+
+    for(Iint i = i_start; i < n; i += i_inc)
+    {
+        Iint const ip = P[ i ];
+        assert( (0 <= ip) && (ip < n) );
+
+        dest[ ip  ] = src[ i ];
+    };
+}
+
+template <typename Iint, typename T>
+static void rf_scatter(hipStream_t streamId, 
+                       Iint const n, 
+                       Iint const * const P, 
+                       T    const * const src, 
+                       T          * const dest )
+{
+    int const nthreads = SCATTER_THREADS;
+    int const max_nblocks = 32*1024;
+    int const nblocks = min( max_nblocks, (n + (nthreads - 1)) / nthreads);
+
+    rf_scatter_kernel<<<dim3(nblocks), dim3(nthreads), 0, streamId>>>(n, P, src, dest );
+}
+
+#endif
+
