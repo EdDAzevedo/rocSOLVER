5fc012f297fa75377930d9a80ff9a7492aee4fed
Ed D'Azevedo <98369794+EdDAzevedo@users.noreply.github.com>
2023-02-02 13:41:26 -0500

    initial checkin

---
+++ library/src/refactor/rocsolver_RfGetMatrixFormat.cpp
@@ -0,0 +1,67 @@
+
+/*! \file */
+/* ************************************************************************
+ * Copyright (C) 2023 Advanced Micro Devices, Inc. All rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ************************************************************************ */
+
+#include "hip_check.h"
+#include "hipsparse_check.h"
+
+#include "rocsolver_refactor.h"
+
+/*
+ -----------------------------------------------------------
+This routine gets the matrix format used in the
+rocsolverRfSetupDevice(), rocsolverRfSetupHost(),
+rocsolverRfResetValues(), rocsolverRfExtractBundledFactorsHost() and
+rocsolverRfExtractSplitFactorsHost() routines.
+ -----------------------------------------------------------
+*/
+
+extern "C" {
+
+rocsolverStatus_t rocsolverRfGetMatrixFormat(rocsolverRfHandle_t handle,
+                                             rocsolverRfMatrixFormat_t* matrix_format,
+                                             rocsolverRfUnitDiagonal_t* diag_format)
+{
+    {
+        bool const isok = (handle != nullptr) && (handle->hipsparse_handle != nullptr);
+        if(!isok)
+        {
+            return (ROCSOLVER_STATUS_NOT_INITIALIZED);
+        };
+    };
+
+    {
+        bool const isok = (matrix_format != nullptr) && (diag_format != nullptr);
+        if(!isok)
+        {
+            return (ROCSOLVER_STATUS_INVALID_VALUE);
+        };
+    };
+
+    *matrix_format = handle->matrix_format;
+    *diag_format = handle->diag_format;
+
+    return (ROCSOLVER_STATUS_SUCCESS);
+};
+};
