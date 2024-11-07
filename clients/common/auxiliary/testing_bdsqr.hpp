39a9d83dd5c0041e0988df0f7cb1b83f7d4790dd
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2024-11-07 13:27:16 -0700

    Minor test improvement

--- clients/common/auxiliary/testing_bdsqr.hpp
+++ clients/common/auxiliary/testing_bdsqr.hpp
@@ -479,9 +479,17 @@ void testing_bdsqr(Arguments& argus)
     rocblas_int hot_calls = argus.iters;
 
     if(argus.alg_mode)
+    {
         EXPECT_ROCBLAS_STATUS(
             rocsolver_set_alg_mode(handle, rocsolver_function_bdsqr, rocsolver_alg_mode_hybrid),
             rocblas_status_success);
+
+        rocsolver_alg_mode alg_mode;
+        EXPECT_ROCBLAS_STATUS(rocsolver_get_alg_mode(handle, rocsolver_function_bdsqr, &alg_mode),
+                              rocblas_status_success);
+
+        EXPECT_EQ(alg_mode, rocsolver_alg_mode_hybrid);
+    }
 
     // check non-supported values
     if(uplo != rocblas_fill_upper && uplo != rocblas_fill_lower)
