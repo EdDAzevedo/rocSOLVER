48212e6946fbf24b4b30a1d295a4c14ac1b44aa3
Eduardo D'Azevedo <Eduardo.DAzevedo@amd.com>
2025-02-18 00:15:34 -0500

    increase xxTRD_BLOCKSIZE

--- library/src/include/ideal_sizes.hpp
+++ library/src/include/ideal_sizes.hpp
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * Copyright (C) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -222,7 +222,7 @@
     when using the blocked algorithm (SYTRD/HETRD). It also applies to the
     corresponding batched and strided-batched routines.*/
 #ifndef xxTRD_BLOCKSIZE
-#define xxTRD_BLOCKSIZE 32
+#define xxTRD_BLOCKSIZE 64
 #endif
 
 /*! \brief Determines the size at which rocSOLVER switches from
